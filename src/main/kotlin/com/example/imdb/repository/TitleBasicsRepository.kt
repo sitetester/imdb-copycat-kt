2db8d4a35ff90bca58a329fe32b36ab0672eb212
sitetester <sitetester2010@gmail.com>
2020-12-29 06:34:24 +0200

    `ORDER BY tr.averageRating` added to query

--- src/main/kotlin/com/example/imdb/repository/TitleBasicsRepository.kt
+++ src/main/kotlin/com/example/imdb/repository/TitleBasicsRepository.kt
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository
 interface TitleBasicsRepository : JpaRepository<TitleBasics, Long> {
 
     // https://thorben-janssen.com/jpql/
-    @Query("SELECT tb, tr FROM TitleBasics tb, TitleRatings tr WHERE tb.tconst = tr.tconst AND tb.genres = :genres")
+    @Query("SELECT tb, tr FROM TitleBasics tb, TitleRatings tr WHERE tb.tconst = tr.tconst AND tb.genres = :genres ORDER BY tr.averageRating")
     fun findByGenres(genres: String, pageable: Pageable): List<Any>
 
 }
